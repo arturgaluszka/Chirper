8827adac63ef82731dd5a181b5177d4fa196e63b
Artur Galuszka <arturgaluszka@gmail.com>
2021-05-08 18:46:31 +0200

    Change aws scripts locations

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 cd /home/ec2-user/server
-sudo /usr/bin/java -jar -Dserver.port=80 \
-    *.jar > /dev/null 2> /dev/null < /dev/null &
+sudo /usr/lib/jvm/java-11-amazon-corretto.x86_64/bin/java -jar \
+  -Dserver.port=80 \
+  *.jar > /dev/null 2> /dev/null < /dev/null &
+
+#sudo /usr/bin/java -jar -Dserver.port=80 \
+#    *.jar > /dev/null 2> /dev/null < /dev/null &
